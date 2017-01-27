5886bb11fafc2d689749f7d6560c4f209779feb4
Dawei Xia <xiadw_public@163.com>
2017-01-27 13:09:24 +0800

    Add a docstring of Movie class

--- P1-MTW/media.py
+++ P1-MTW/media.py
@@ -1,5 +1,14 @@
 class Movie():
     def __init__(self, title, storyline, poster_image, trailer_youtube):
+        '''
+        Initialize a Movie
+
+        Keyword arguments:
+        title -- movie title
+        storyline -- movie storyline
+        poster_image -- url link of the movie's poster image
+        trailer_youtube -- url link of the movie trailer on youtube
+        '''
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image
